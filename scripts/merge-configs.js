25dad6dd51f4d9cac100549c3429520965fc9620
kaiiiz <at881005@gmail.com>
2019-04-28 00:29:12 +0800

    use data/book.yml replace config rather than merge config

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -2,25 +2,9 @@
 
 'use strict';
 
-var merge = require('./merge');
-
-hexo.on('generateBefore', function() {
+hexo.on('generateBefore', function () {
   if (hexo.locals.get) {
-    var data = hexo.locals.get('data');
-    /**
-     * Merge configs from _data/book.yml into hexo.theme.config.
-     * If `override`, configs in book.yml will rewrite configs in hexo.theme.config.
-     * If book.yml not exists, merge all `theme_config.*` into hexo.theme.config.
-     */
-    if (data && data.book) {
-      if (data.book.override) {
-        hexo.theme.config = data.book;
-      } else {
-        merge(hexo.config, data.book);
-        merge(hexo.theme.config, data.book);
-      }
-    } else {
-      merge(hexo.theme.config, hexo.config.theme_config);
-    }
+    var data = hexo.locals.get('data')
+    data && data.book && (hexo.theme.config = data.book)
   }
-});
+})
