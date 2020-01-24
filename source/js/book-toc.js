cc587cb283848561300d3d6e53a9a63b790197d8
kaiiiz <at881005@gmail.com>
2020-01-24 06:45:52 +0000

    archive style & dynamic update maxHeight of post info

--- source/js/book-toc.js
+++ source/js/book-toc.js
@@ -1,6 +1,6 @@
 tocbot.init({
     tocSelector: '.book-tocbot',
-    contentSelector: '.book-post',
+    contentSelector: '.book-content',
     headingSelector: 'h1, h2, h3, h4, h5',
     collapseDepth: 2,
     orderedList: false,
@@ -11,7 +11,7 @@ function expand_toc() {
     var b = document.querySelector(".book-toc-expand");
     tocbot.init({
         tocSelector: '.book-tocbot',
-        contentSelector: '.book-post',
+        contentSelector: '.book-content',
         headingSelector: 'h1, h2, h3, h4, h5',
         collapseDepth: 6,
         orderedList: false,
@@ -25,7 +25,7 @@ function collapse_toc() {
     var b = document.querySelector(".book-toc-expand");
     tocbot.init({
         tocSelector: '.book-tocbot',
-        contentSelector: '.book-post',
+        contentSelector: '.book-content',
         headingSelector: 'h1, h2, h3, h4, h5',
         collapseDepth: 2,
         orderedList: false,
@@ -37,33 +37,28 @@ function collapse_toc() {
 
 function go_top() {
     window.scrollTo(0, 0);
+    setTimeout(update_maxHeight, 150); // wait animation
 }
 
 function go_bottom() {
     window.scrollTo(0, document.body.scrollHeight);
+    setTimeout(update_maxHeight, 150); // wait animation
 }
 
 function get_maxHeight() {
     let meta = document.querySelector('.book-post-meta')
     let tocbot_menu = document.querySelector('.book-tocbot-menu')
 
-    let meta_pos = meta.getBoundingClientRect();
-    let tocbot_menu_pos = tocbot_menu.getBoundingClientRect();
+    let meta_pos = (meta == null) ? new DOMRect(0, 0, 0, 0) : meta.getBoundingClientRect();
+    let tocbot_menu_pos = (tocbot_menu == null) ? new DOMRect(0, 0, 0, 0) : tocbot_menu.getBoundingClientRect();
 
-    let screenHeight = screen.availHeight;
-    let maxHeight = screenHeight - meta_pos.y - meta_pos.height - tocbot_menu_pos.height - screenHeight * 0.3;
+    let screenHeight = window.innerHeight;
+    let maxHeight = screenHeight - meta_pos.y - meta_pos.height - tocbot_menu_pos.height - screenHeight * 0.15;
 
     return maxHeight;
 }
 
-window.onload = () => {
-    let tocbot = document.querySelector('.book-tocbot')
-    let maxHeight = get_maxHeight();
-    tocbot.style.maxHeight = maxHeight + "px"
-}
-
-// post metadata
-window.onscroll = () => {
+function update_maxHeight() {
     let scrollY = window.scrollY;
     let meta = document.querySelector('.book-post-meta')
     let tocbot = document.querySelector('.book-tocbot')
@@ -81,3 +76,12 @@ window.onscroll = () => {
         tocbot.style.maxHeight = maxHeight + "px"
     }
 }
+
+window.onload = () => {
+    let tocbot = document.querySelector('.book-tocbot')
+    let maxHeight = get_maxHeight();
+    tocbot.style.maxHeight = maxHeight + "px"
+}
+
+window.onresize = update_maxHeight
+window.onscroll = update_maxHeight
