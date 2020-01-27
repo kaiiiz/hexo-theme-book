28c9667ca9da47a20608e042e177baf9907a7cae
kaiiiz <at881005@gmail.com>
2020-01-27 07:02:40 +0000

    functional programming

--- source/js/book-menu.js
+++ source/js/book-menu.js
@@ -31,66 +31,76 @@ function collapse(name, body, i) {
     accordion.appendChild(acco_body);
 
     name.parentNode.replaceChild(accordion, name);
-
-    return accordion;
 }
 
 // clear invalid syntax
-document.querySelectorAll('.book-menu > :not(ul):not(h1):not(h2):not(h3):not(h4):not(h5):not(h6)').forEach((e) => {
-    e.parentNode.removeChild(e);
-})
+function clear_invalid_syntax() {
+    document.querySelectorAll('.book-menu > :not(ul):not(h1):not(h2):not(h3):not(h4):not(h5):not(h6)').forEach((e) => {
+        e.parentNode.removeChild(e);
+    })
+}
 
 // pack accordion
-document.querySelectorAll('.book-menu > ul').forEach((e, idx) => {
-    var sibling = e.previousElementSibling;
-    while (sibling != null) {
-        if (sibling.tagName == "H1" || sibling.tagName == "H2" ||
-            sibling.tagName == "H3" || sibling.tagName == "H4" ||
-            sibling.tagName == "H5" || sibling.tagName == "H6" ||
-            sibling.classList.contains('accordion')) {
-            break;
-        }
-        sibling = sibling.previousElementSibling;
-    }
-    if (!sibling || sibling.tagName == "H1") {
-        e.classList.add('uncollapsible');
-    }
-    else {
-        accordion = collapse(sibling, e, idx);
-    }
-})
-
-// highlight current tab
-document.querySelectorAll('.book-menu a').forEach((item) => {
-    if (!item.getAttribute('href')) return // if href has no value
-    // normalized url
-    let sharp = window.location.href.search('#');
-    let url = window.location.href
-    if (sharp != -1) {
-        url = url.slice(0, sharp);
-    }
-    if (url.slice(-1) == '/') {
-        url = url.slice(0, -1);
-    }
-    if (item.href === url) {
-        item.classList.add('current-tab')
-        var parent = item.parentNode;
-        while (parent.className != "book-menu") {
-            if (parent.className == "accordion") {
+function pack_menu_accordion() {
+    document.querySelectorAll('.book-menu > ul').forEach((e, idx) => {
+        var sibling = e.previousElementSibling;
+        while (sibling != null) {
+            if (sibling.tagName == "H1" || sibling.tagName == "H2" ||
+                sibling.tagName == "H3" || sibling.tagName == "H4" ||
+                sibling.tagName == "H5" || sibling.tagName == "H6") {
                 break;
             }
-            parent = parent.parentNode;
+            sibling = sibling.previousElementSibling;
         }
-        if (parent.className == "accordion") {
-            parent.querySelector('input').setAttribute("checked", "");
+        if (!sibling || sibling.tagName == "H1") {
+            e.classList.add('uncollapsible');
         }
-    }
-})
+        else {
+            collapse(sibling, e, idx);
+        }
+    })
+}
 
-window.onbeforeunload = () => {
+// highlight current tab
+function highlight_current_tab() {
+    document.querySelectorAll('.book-menu a').forEach((item) => {
+        if (!item.getAttribute('href')) return // if href has no value
+        // normalized url
+        let sharp = window.location.href.search('#');
+        let url = window.location.href
+        if (sharp != -1) {
+            url = url.slice(0, sharp);
+        }
+        if (url.slice(-1) == '/') {
+            url = url.slice(0, -1);
+        }
+        if (item.href === url) {
+            item.classList.add('current-tab')
+            var parent = item.parentNode;
+            while (parent.className != "book-menu") {
+                if (parent.className == "accordion") {
+                    break;
+                }
+                parent = parent.parentNode;
+            }
+            if (parent.className == "accordion") {
+                parent.querySelector('input').setAttribute("checked", "");
+            }
+        }
+    })
+}
+
+/* ----- onload ----- */
+
+clear_invalid_syntax()
+pack_menu_accordion()
+highlight_current_tab()
+
+// restore sidebar position after reloading page
+window.addEventListener('beforeunload', () => {
     let sidebarPos = document.querySelector('.book-menu').scrollTop
     window.localStorage.setItem('sidebarPos', sidebarPos)
-}
+})
 
 if (window.localStorage.sidebarPos) {
     let sidebarPos = window.localStorage.sidebarPos
