9fafef39156106ec323fa6fcd3c2e57f521dee1f
kaiiiz <at881005@gmail.com>
2020-03-25 14:41:48 +0800

    Organize toggle code

--- source/js/book.js
+++ source/js/book.js
@@ -11,19 +11,3 @@ function open_sidebar() {
     sidebar.classList.add('show')
     overlay.classList.add('show')
 }
-
-function sidebar_toggle() {
-    let sidebar_toggle = document.querySelector('.sidebar-toggle')
-    let sidebar = document.querySelector('.book-sidebar')
-    let content = document.querySelector('.off-canvas-content')
-    if (sidebar_toggle.classList.contains('extend')) { // show
-        sidebar_toggle.classList.remove('extend')
-        sidebar.classList.remove('hide')
-        content.classList.remove('extend')
-    }
-    else { // hide
-        sidebar_toggle.classList.add('extend')   
-        sidebar.classList.add('hide')
-        content.classList.add('extend')
-    }
-}
